4058b43fc6c1eec86e6b5f61f6af501742c9a01b
Isaac Clancy <ikec4@yahoo.com>
2019-07-03 18:25:12 +1200

    Added ability to add and remove threads from a ThreadBarrier

--- DX12Engine/ThreadBarrier.h
+++ DX12Engine/ThreadBarrier.h
@@ -1,33 +1,80 @@
 #pragma once
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 
 class ThreadBarrier
 {
-	std::mutex mMutex;
+	std::mutex mutex;
 	std::condition_variable conditionVariable;
-	unsigned int mWaitingCount = 0u;
-	unsigned int mGeneration = 0u;
-public:
-	void sync(unsigned int maxThreads);
+	unsigned int generation = 0u;
+	unsigned long waitingCount = 0u;
+	std::atomic<unsigned long> threadCount;
+	long increaseInThreads = 0u;
 
-	template<class OnLastThread>
-	void sync(unsigned int maxThreads, OnLastThread onLastThread)
+	void syncHelper(std::unique_lock<std::mutex> lock)
 	{
-		std::unique_lock<std::mutex> lock(mMutex);
-		++mWaitingCount;
-		if (mWaitingCount == maxThreads)
+		++waitingCount;
+		auto oldThreadCount = threadCount.load(std::memory_order_relaxed) & 0x7fffffff;
+		if (waitingCount == oldThreadCount)
 		{
-			onLastThread();
-			mWaitingCount = 0u;
-			++mGeneration;
+			threadCount.store(oldThreadCount + increaseInThreads, std::memory_order_relaxed); //unlock and increase by increaseInThreads
+			increaseInThreads = 0u;
+			waitingCount = 0u;
+			++generation;
 			lock.unlock();
 			conditionVariable.notify_all();
 		}
 		else
 		{
-			conditionVariable.wait(lock, [&gen = mGeneration, oldGen = mGeneration]() { return gen != oldGen; });
+			conditionVariable.wait(lock, [&gen = generation, oldGen = generation]() { return gen != oldGen; });
 			lock.unlock();
 		}
+	}
+public:
+	ThreadBarrier(unsigned long threadCount1) : threadCount(threadCount1) {}
+
+	void sync()
+	{
+		syncHelper(std::unique_lock<std::mutex>(mutex));
+	}
+
+	void syncAndRemoveThread()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		--increaseInThreads;
+		syncHelper(std::move(lock));
+	}
+
+	void addThread()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		bool threadCountIsLocked = false;
+		auto oldThreadCount = threadCount.load(std::memory_order_relaxed);
+		do
+		{
+			if ((oldThreadCount & 0x80000000) != 0)
+			{
+				threadCountIsLocked = true;
+				break;
+			}
+		} while (threadCount.compare_exchange_weak(oldThreadCount, oldThreadCount + 1u, std::memory_order_relaxed, std::memory_order_relaxed));
+
+		if (threadCountIsLocked)
+		{
+			++increaseInThreads;
+			conditionVariable.wait(lock, [&gen = generation, oldGen = generation]() { return gen != oldGen; });
+		}
+	}
+
+	unsigned int lockAndGetThreadCount()
+	{
+		auto oldThreadCount = threadCount.load(std::memory_order_relaxed);
+		if ((oldThreadCount & 0x80000000) == 0)
+		{
+			//The threadCount is unlocked. Atomically lock it and get its value.
+			oldThreadCount = threadCount.fetch_or(0x80000000, std::memory_order_relaxed);
+		}
+		return oldThreadCount & 0x7fffffff;
 	}
 };
