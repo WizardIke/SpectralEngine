0c3694a131a0adaf5fa82c66c0cdb88ba6740257
Isaac Clancy <ikec4@yahoo.com>
2019-07-30 17:46:43 +1200

    added CoInitialize and CoUnitialize to main

--- DemoGame/main.cpp
+++ DemoGame/main.cpp
@@ -7,10 +7,27 @@ INT WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 int main()
 #endif
 {
+#ifdef _WIN32
+	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+#endif
 	try
 	{
 		GlobalResources game{};
 		game.start();
+#ifdef _WIN32
+		CoUninitialize();
+#endif
 	}
-	catch(GraphicsAdapterNotFound) {}
+	catch(GraphicsAdapterNotFound) 
+	{
+#ifdef _WIN32
+		CoUninitialize();
+#endif
+	}
+	catch (...)
+	{
+#ifdef _WIN32
+		CoUninitialize();
+#endif
+	}
 }
