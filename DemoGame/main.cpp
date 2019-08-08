eb0aaa3943679ce08cba6a2b23f6bc727ba69d13
Isaac Clancy <ikec4@yahoo.com>
2019-08-08 18:50:01 +1200

    made initializing and uninitializing com exception safe in main

--- DemoGame/main.cpp
+++ DemoGame/main.cpp
@@ -2,32 +2,36 @@
 #include <GraphicsAdapterNotFound.h>
 
 #ifdef _WIN32
+namespace
+{
+	struct ComEnvironment
+	{
+		ComEnvironment()
+		{
+			CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+		}
+
+		~ComEnvironment()
+		{
+			CoUninitialize();
+		}
+	};
+}
+#endif
+
+#ifdef _WIN32
 INT WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 #else
 int main()
 #endif
 {
 #ifdef _WIN32
-	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	ComEnvironment com;
 #endif
 	try
 	{
 		GlobalResources game{};
 		game.start();
-#ifdef _WIN32
-		CoUninitialize();
-#endif
 	}
-	catch(GraphicsAdapterNotFound) 
-	{
-#ifdef _WIN32
-		CoUninitialize();
-#endif
-	}
-	catch (...)
-	{
-#ifdef _WIN32
-		CoUninitialize();
-#endif
-	}
+	catch(GraphicsAdapterNotFound) {}
 }
